7658c7af7d872f6c72062bcef007479672ea764a
Ismail Arafa <ismailarafa18@gmail.com>
2018-09-08 20:20:43 +0200

    change redirect uri

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "f8df77a3e85c4e49b6e702e8be74f262";
 const SCOPES = encodeURIComponent("user-read-currently-playing");
-const REDIRECT_URI = encodeURIComponent("http://localhost:3000/callback");
+const REDIRECT_URI = encodeURIComponent("https://react-redux-spotty-genius.surge.sh/callback");
 const URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=${SCOPES}&redirect_uri=${REDIRECT_URI}&show_dialog=true`;
 
 export default URL;
